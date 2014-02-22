d51c674d3e5fb0626b5b3196c2aadaec1775253c
michalliu <michalliu@foxmail.com>
2014-02-22 21:49:28 +0800

    remove devmon.sh it will run after every successful login, thus load multiple times which is very bad

--- common/etc/profile.d/devmon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# https://wiki.debian.org/DotFiles
-# http://www.bo.infn.it/alice/alice-doc/mll-doc/linux/scripts/node2.html
-
-# auto mount/umount removable device
-/usr/bin/devmon | logger -t drvmon &
